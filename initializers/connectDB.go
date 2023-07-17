7a11512f224ee5dab28443b92a331a44840dd389
e-phraim <ephraimetuk85@gmail.com>
2023-07-17 08:05:08 +0100

    Install uuid-ossp Extension

--- initializers/connectDB.go
+++ initializers/connectDB.go
@@ -18,5 +18,12 @@ func ConnectDB(config *Config) {
 	if err != nil {
 		log.Fatal("Failed To Connect To The Database")
 	}
+
+	installExtension := "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\""
+	err = DB.Exec(installExtension).Error
+	if err != nil{
+		log.Fatal("Failed to install uuid-ossp extension")
+	}
+
 	fmt.Println("? Connected Successfully To The Database")
 }
